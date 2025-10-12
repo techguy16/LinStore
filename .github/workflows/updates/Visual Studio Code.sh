4d7b31ef0cfcfd544916b868f50b0539f9a53fda
techguy16 <88870951+techguy16@users.noreply.github.com>
2025-10-13 12:17:37 +1300

    Update Visual Studio Code.sh

--- .github/workflows/updates/Visual Studio Code.sh
+++ .github/workflows/updates/Visual Studio Code.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+curl -s https://corsproxy.io/?url=https://api.github.com/repos/microsoft/vscode/releases/latest 
 curl -s https://corsproxy.io/?url=https://api.github.com/repos/microsoft/vscode/releases/latest | grep tag_name  | awk '{print $2}' | sed 's/\"//g' | sed "s/,//"
